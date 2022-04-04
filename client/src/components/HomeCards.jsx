b80ddbdcb0853917da1b26653a2d190e1c79675b
Harold Arriola Nucamendi <arriolaharold2001@gmail.com>
2022-04-04 19:19:09 -0400

    added SQL DB and created homepage components-- working on small sections styling

--- client/src/components/HomeCards.jsx
+++ client/src/components/HomeCards.jsx
@@ -2,12 +2,38 @@ import React from "react";
 import "./homeCards.scss";
 
 const HomeCards = ({ data }) => {
-  const modeS = "modelS";
+  console.log(data);
   return (
     <div>
-      <section className={`hero-card hero-card_${modeS} font-link`}>
-        <h1>Hello</h1>
-      </section>
+      {data.map((item) => (
+        <section
+          className={`hero-card hero-card_${item.id} font-link`}
+          key={item.id}
+        >
+          <div className="hero-card_description">
+            <h1>{item.car_model}</h1>
+            <p>Order Online for Touchless Delivery</p>
+          </div>
+
+          <span role="button" className="animate-chevron_bounce">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="chevron-down"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19 9l-7 7-7-7"
+              />
+            </svg>
+          </span>
+        </section>
+      ))}
+      ;
     </div>
   );
 };
