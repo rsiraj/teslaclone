f803e69f138c75e74ff234673487a648d758310a
Harold Arriola Nucamendi <arriolaharold2001@gmail.com>
2022-04-06 10:53:28 -0400

    added buttons to hero cards for cars

--- client/src/components/HomeCards.jsx
+++ client/src/components/HomeCards.jsx
@@ -12,9 +12,9 @@ const HomeCards = ({ data }) => {
           key={item.id}
         >
           <div className="hero-card_description">
-            <h1>{item.car_model}</h1>
+            <h1>{item.item_model}</h1>
             <div>
-              <span>{item.car_description}</span>
+              <span>{item.item_description}</span>
               <a href="https://www.tesla.com/support/taking-delivery?redirect=no">
                 <span className="link-description">
                   {item.id < 5 ? "Touchless Delivery" : ""}
@@ -27,10 +27,24 @@ const HomeCards = ({ data }) => {
               id={`${item.id >= 5 ? "hidden" : ""}`}
             >
               <div className="button-group">
-                <a href="#" className="custom-btn">
+                <a href={item.item_order_btn} className="custom-btn">
                   Custom Order
                 </a>
-                <a href="#" className="inventory-btn">
+                <a href={item.item_inventory_btn} className="inventory-btn">
+                  Existing Inventory
+                </a>
+              </div>
+            </div>
+
+            <div
+              className="product-btn "
+              id={`${!(item.id >= 5) ? "hidden" : ""}`}
+            >
+              <div className="button-group">
+                <a href={item.item_order_btn} className="custom-btn">
+                  Custom Order
+                </a>
+                <a href={item.item_inventory_btn} className="inventory-btn">
                   Existing Inventory
                 </a>
               </div>
@@ -55,14 +69,6 @@ const HomeCards = ({ data }) => {
               </svg>
             </span>
           </a>
-          {/* {data.type === "solar"
-            ? data.map((item) => (
-                <div className="hero-card_description">
-                  <h1>{item.car_model}</h1>
-                  <p>Order Online for Touchless Delivery</p>
-                </div>
-              ))
-            : ""} */}
         </section>
       ))}
       ;
