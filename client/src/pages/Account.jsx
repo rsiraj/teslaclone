ee304f86e291c6edd47d3eb40ba1e6e31b822c80
Daniel Escamilla <descamilla128@gmail.com>
2022-04-06 14:19:37 -0400

    first commit

--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Account = () => {
   return (
-    <div className='signin'>
-        <div className='signin_header'>
-            <div className="sign_logo">
-                
-            </div>
+    <div className="signin">
+      <div className="signin_header">
+        <div className="sign_logo">
+          {/* <Link>
+            <img
+              src="./img/tesla-logo-header.png"
+              alt="tesla"
+              className="header-logo"
+            />
+          </Link> */}
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Account
+export default Account;
