fe0498111fd62fdc2b73ac4d0e49fd292421bdad
Emassey123 <94918775+Emassey123@users.noreply.github.com>
2022-04-06 20:40:55 -0400

    starting the safety component but running into errors

---
+++ client/src/components/Safety.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Safety.scss";
+const Safety = () => {
+  return (
+    <div>
+      <div className="safety-container">
+        <h3></h3>
+        <h1></h1>
+        <p></p>
+      </div>
+      <div className="safety-image">
+        <img src="../../public/img/MS-Safety-Hero-Desktop.jpeg" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Safety;
--- client/src/pages/ModelS.jsx
+++ client/src/pages/ModelS.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import CarHero from "../components/CarHero";
+import Safety from "../components/Safety";
 
 const ModelS = () => {
   return (
     <div>
       <CarHero />
+      <Safety />
     </div>
   );
 };
