2c50ad817864f0c96fb6191b0fbe8d1c447ded9f
Rehan Siraj <rsiraj@r2hstudent.org>
2022-04-08 11:19:43 -0400

    added scroll to top

--- client/src/pages/ModelX.jsx
+++ client/src/pages/ModelX.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import CarHero from "../components/CarHero";
 import Safety from "../components/Safety";
 import Order from "../components/Order";
+import ScrollToTop from "react-scroll-to-top";
+import {ReactComponent as MySVG} from "../arrow.svg"
+
 const ModelX = ({ data }) => {
   return (
     <div>
       <CarHero data={data} />
       <Safety data={data} />
       <Order />
+      <ScrollToTop smooth component={<MySVG />} />
     </div>
   );
 };
